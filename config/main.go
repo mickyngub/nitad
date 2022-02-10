a904ae627783e54ea1225b6e148f2267e342743b
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-10 16:45:13 +0700

    feat: loadenv config

---
+++ config/main.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func Loadenv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file!")
+	}
+}
