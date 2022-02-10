a23e7cb43b65a7fdb87071c0a4468e5b04a1bd48
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-10 16:45:43 +0700

    feat: database package

--- database/main.go
+++ database/main.go
@@ -12,19 +12,33 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client *mongo.Client
 
-COLLECTIONS := map[string]string{
+const DatabaseName = "nitad"
+
+var COLLECTIONS = map[string]string{
 	"PROJECT":     "project",
 	"CATEGORY":    "category",
 	"SUBCATEGORY": "subcategory",
 	"ADMIN":       "admin",
 }
 
-func connectDb() (*mongo.Client, []string) {
+func GetCollection(collectionName string) (*mongo.Collection, context.Context) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	collection := client.Database(DatabaseName).Collection(collectionName)
+
+	return collection, ctx
+}
+
+func GetClient() *mongo.Client {
+	return client
+}
+
+func ConnectDb() {
 	MONGO_URI := os.Getenv("MONGO_URI")
 
-	fmt.Println(COLLECTIONS)
-	client, err := mongo.NewClient(options.Client().ApplyURI(MONGO_URI))
+	var err error
+	client, err = mongo.NewClient(options.Client().ApplyURI(MONGO_URI))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,15 +48,17 @@ func connectDb() (*mongo.Client, []string) {
 		log.Fatal(err)
 	}
 
-	/*
-	   List databases
-	*/
+	// TODO: fix loop bug db
+
+	// List databases
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
-
-	return client, databases
 }
 
+func DisconnectDb() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client.Disconnect(ctx)
+}
