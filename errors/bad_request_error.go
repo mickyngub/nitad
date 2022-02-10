61f6e6aaf0da76a426724ba33093ffae36a5af07
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-10 17:40:33 +0700

    feat: bad_request_error

---
+++ errors/bad_request_error.go
@@ -0,0 +1,22 @@
+package errors
+
+import "github.com/gofiber/fiber/v2"
+
+type badRequestError struct {
+	message string
+}
+
+// NewBadRequestError used when client send wrong type of input
+// e.g. expect application/json but got plain/text
+// 			or wrong username/password
+func NewBadRequestError(message string) CustomError {
+	return &badRequestError{message}
+}
+
+func (b *badRequestError) Code() int {
+	return fiber.StatusBadRequest
+}
+
+func (b *badRequestError) Message() string {
+	return b.message
+}
