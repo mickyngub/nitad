fb501fb76cbd34a03723a4187d158f9bef62c5fd
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-10 16:48:01 +0700

    feat: subcategory entity

---
+++ api/subcategory/entity.go
@@ -0,0 +1,9 @@
+package subcategory
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type subcategory struct {
+	ID     primitive.ObjectID `bson:"_id,omitempty`
+	Title  string             `bson:"title,omitempty`
+	Images string             `bson:"images,omitempty`
+}
