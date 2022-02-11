46322e289a094b16c1af77a0292faa8087799b7d
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-11 14:53:41 +0700

    re: getSubcategoryById

--- api/subcategory/controller.go
+++ api/subcategory/controller.go
@@ -8,8 +8,6 @@ import (
 	"github.com/birdglove2/nitad-backend/errors"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func NewController(
@@ -36,21 +34,16 @@ var collectionName = database.COLLECTIONS["SUBCATEGORY"]
 
 // Get subcategory by id
 func (contc *Controller) Getsubcategory(c *fiber.Ctx) error {
+	subcategoryId := c.Params("subcategoryId")
 
-	collection, ctx := database.GetCollection(collectionName)
-
-	id, err := primitive.ObjectIDFromHex(c.Params("subcategoryId"))
+	objectId, err := isValidObjectId(subcategoryId)
 	if err != nil {
-		return errors.Throw(c, errors.NewNotFoundError("id is in valid 2"))
+		return errors.Throw(c, err)
 	}
 
 	var result bson.M
-	err = collection.FindOne(ctx, bson.D{{"_id", id}}).Decode(&result)
-	if err != nil {
-		// ErrNoDocuments means that the filter did not match any documents in the collection
-		if err == mongo.ErrNoDocuments {
-			return errors.Throw(c, errors.NewNotFoundError("subcategoryId"))
-		}
+	if result, err = findById(objectId); err != nil {
+		return errors.Throw(c, err)
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"result": result})
@@ -92,16 +85,16 @@ func (contc *Controller) Addsubcategory(c *fiber.Ctx) error {
 
 	insertRes, insertErr := collection.InsertOne(ctx, bson.D{
 		{Key: "title", Value: p.Title},
-		{Key: "images", Value: p.Images},
+		{Key: "image", Value: p.Image},
 	})
 	if insertErr != nil {
 		log.Fatal(insertErr)
 	}
 
 	res := map[string]interface{}{
-		"id":     insertRes.InsertedID,
-		"title":  p.Title,
-		"images": p.Images,
+		"id":    insertRes.InsertedID,
+		"title": p.Title,
+		"image": p.Image,
 	}
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"result": res})
 
---
+++ api/subcategory/function.go
@@ -0,0 +1,34 @@
+package subcategory
+
+import (
+	"github.com/birdglove2/nitad-backend/database"
+	"github.com/birdglove2/nitad-backend/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func isValidObjectId(id string) (primitive.ObjectID, errors.CustomError) {
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return objectId, errors.NewBadRequestError("Invalid objectId")
+	}
+	return objectId, nil
+}
+
+func findById(id primitive.ObjectID) (bson.M, errors.CustomError) {
+	collection, ctx := database.GetCollection(collectionName)
+
+	var result bson.M
+	err := collection.FindOne(ctx, bson.D{{"_id", id}}).Decode(&result)
+	if err != nil {
+		// ErrNoDocuments means that the filter did not match any documents in the collection
+		if err == mongo.ErrNoDocuments {
+			return result, errors.NewNotFoundError("subcategoryId")
+		} else {
+			return result, errors.NewBadRequestError("something went wrong")
+		}
+	}
+
+	return result, nil
+}
