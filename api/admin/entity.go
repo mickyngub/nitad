df3a68117a3194da00fb7af7d7e2465ac5b2f9cb
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-18 18:10:57 +0700

    feat: admin entity

--- api/admin/entity.go
+++ api/admin/entity.go
@@ -1,9 +1,20 @@
 package admin
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/birdglove2/nitad-backend/database"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var collectionName = database.COLLECTIONS["ADMIN"]
 
 type Admin struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty`
-	Username string             `bson:"username,omitempty`
-	Password []string           `bson:"password,omitempty`
+	ID       primitive.ObjectID `bson:"_id" json:"id"`
+	Username string             `bson:"username" json:"username" form:"username" validate:"required"`
+	Password string             `bson:"password" json:"password" form:"password" validate:"required"`
+}
+
+type AdminSignup struct {
+	Username        string `form:"username" validate:"required"`
+	Password        string `form:"password" validate:"required"`
+	ConfirmPassword string `form:"confirmPassword" validate:"required"`
 }
