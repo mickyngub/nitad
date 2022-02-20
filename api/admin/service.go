7c19bee64303427f0515454892ebc2197294f3ef
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-20 19:22:16 +0700

    feat: check existing admin

--- api/admin/service.go
+++ api/admin/service.go
@@ -5,12 +5,38 @@ import (
 	"github.com/birdglove2/nitad-backend/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func FindByUsername(username string) (*Admin, errors.CustomError) {
+	collection, ctx := database.GetCollection(collectionName)
+
+	var admin Admin
+	err := collection.FindOne(ctx, bson.D{{Key: "username", Value: username}}).Decode(&admin)
+	if err != nil {
+		// ErrNoDocuments means that the filter did not match any documents in the collection
+		if err == mongo.ErrNoDocuments {
+			return nil, errors.NewNotFoundError(collectionName)
+		} else {
+			return nil, errors.NewInternalServerError("getting user error, " + err.Error())
+		}
+	}
+
+	return &admin, nil
+}
 
 func CreateAdmin(a Admin) (Admin, errors.CustomError) {
 	collection, ctx := database.GetCollection(collectionName)
 
-	// TODO: check for existing one
+	admin, err := FindByUsername(a.Username)
+	if err != nil && err.Error() != errors.NewNotFoundError(collectionName).Error() {
+		return a, err
+	}
+
+	if admin != nil {
+		return a, errors.NewBadRequestError("Username has already been taken")
+	}
+
 	insertRes, insertErr := collection.InsertOne(ctx, bson.D{
 		{Key: "username", Value: a.Username},
 		{Key: "password", Value: a.Password},
