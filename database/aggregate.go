69409fc452c500e0a2c0144fa85d9a73237c2eb4
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-20 19:21:57 +0700

    re: match stage

--- database/aggregate.go
+++ database/aggregate.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -24,6 +23,6 @@ func AppendUnsetStage(pipe mongo.Pipeline, field string) mongo.Pipeline {
 	return append(pipe, bson.D{{Key: "$unset", Value: field}})
 }
 
-func AppendMatchStage(pipe mongo.Pipeline, field string, oid primitive.ObjectID) mongo.Pipeline {
-	return append(pipe, bson.D{{Key: "$match", Value: bson.D{{Key: field, Value: oid}}}})
+func AppendMatchStage(pipe mongo.Pipeline, field string, value interface{}) mongo.Pipeline {
+	return append(pipe, bson.D{{Key: "$match", Value: bson.D{{Key: field, Value: value}}}})
 }
