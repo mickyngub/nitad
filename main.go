68fb64d8861523f975cefef0ebcfa7dbd2c907bd
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-21 23:57:08 +0700

    feat: new version

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	api.CreateAPI(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": "Hello, this is NITAD Backend Server v1.3 !"})
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": "Hello, this is NITAD Backend Server v1.4 !"})
 	})
 
 	app.All("*", func(c *fiber.Ctx) error {
