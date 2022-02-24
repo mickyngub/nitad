7b520a30b8f683c22f7036719111c437d32539c0
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-02-24 20:29:02 +0700

    Merge 'dev' to 'main', restructure category link with sucate

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	cronjob.Init()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": "Hello, this is NITAD Backend Server v1.6 !"})
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": "Hello, this is NITAD Backend Server v1.7 !"})
 	})
 
 	app.All("*", func(c *fiber.Ctx) error {
