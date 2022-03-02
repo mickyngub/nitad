cd7ac689dda27181695597c992826d48d471d18f
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-02 22:10:21 +0700

    feat: spatial controller CRUD

--- api/spatial/controller.go
+++ api/spatial/controller.go
@@ -1,8 +1,9 @@
 package spatial
 
 import (
-	"strconv"
+	"log"
 
+	"github.com/birdglove2/nitad-backend/api/admin"
 	"github.com/birdglove2/nitad-backend/errors"
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,33 +14,84 @@ func NewController(
 
 	controller := &Controller{}
 
-	spatialRoute.Get("/:roomNumber", controller.GetLink)
+	spatialRoute.Get("/", controller.GetSpatial)
+
+	spatialRoute.Use(admin.IsAuth())
+	spatialRoute.Post("/", AddAndEditSpatialValidator, controller.AddSpatial)
+	spatialRoute.Put("/", AddAndEditSpatialValidator, controller.EditSpatial)
+	spatialRoute.Delete("/", controller.DeleteSpatial)
 }
 
 type Controller struct{}
 
-var links = []string{
-	"https://google.com",
-	"https://facebook.com",
-	"https://youtube.com",
-	"https://google.com",
-	"https://facebook.com",
-	"https://youtube.com",
-	"https://google.com",
-	"https://facebook.com",
-	"https://google.com",
-	"https://facebook.com",
+// get that one spatial in the db
+func (contc *Controller) GetSpatial(c *fiber.Ctx) error {
+	spatial, err := GetOneSpatial()
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": spatial})
 }
 
-// get spatial new link
-func (contc *Controller) GetLink(c *fiber.Ctx) error {
-	roomNumberStr := c.Params("roomNumber")
-	roomNumber, err := strconv.Atoi(roomNumberStr)
-	if err != nil || roomNumber < 1 || roomNumber > 10 {
-		return errors.Throw(c, errors.NewBadRequestError("room number only valid from 1 to 10"))
+// add that one spatial in the db
+func (contc *Controller) AddSpatial(c *fiber.Ctx) error {
+	_, err := GetOneSpatial()
+	if err != nil && err.Error() != "no spatial link yet, please create one first" {
+		return errors.Throw(c, err)
 	}
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": fiber.Map{
-		"roomNumber": roomNumber,
-		"link":       links[roomNumber-1],
-	}})
+	if err == nil { // meaning that there is spatial link passed
+		return errors.Throw(c, errors.NewBadRequestError("Can only create 1 spatial link, please edit it instead"))
+	}
+
+	parseSpatial, ok := c.Locals("spatialBody").(*Spatial)
+	if !ok {
+		return errors.Throw(c, errors.NewInternalServerError("Add Spatial went wrong!"))
+	}
+
+	log.Println(parseSpatial)
+	newSpatial, err := Add(parseSpatial)
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": newSpatial})
+}
+
+// edit that one spatial in the db
+func (contc *Controller) EditSpatial(c *fiber.Ctx) error {
+	spatial, err := GetOneSpatial()
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	parseSpatial, ok := c.Locals("spatialBody").(*Spatial)
+	if !ok {
+		return errors.Throw(c, errors.NewInternalServerError("Edit Spatial went wrong!"))
+
+	}
+
+	spatial.Link = parseSpatial.Link
+
+	updatedSpatial, err := Edit(&spatial)
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": updatedSpatial})
+}
+
+// delete that one spatial in the db
+func (contc *Controller) DeleteSpatial(c *fiber.Ctx) error {
+	spatial, err := GetOneSpatial()
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	err = Delete(spatial.ID)
+	if err != nil {
+		return errors.Throw(c, err)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true, "result": "Delete spatial " + spatial.Link + " successfully!"})
 }
