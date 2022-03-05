351c629c96cc198824f310491aa39e7f580f2066
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-05 17:01:56 +0700

    feat: connection route

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/birdglove2/nitad-backend/api/admin"
 	"github.com/birdglove2/nitad-backend/api/category"
+	"github.com/birdglove2/nitad-backend/api/connection"
 	"github.com/birdglove2/nitad-backend/api/project"
 	"github.com/birdglove2/nitad-backend/api/search"
 	"github.com/birdglove2/nitad-backend/api/spatial"
@@ -18,6 +19,7 @@ func CreateAPI(app *fiber.App, gcpService gcp.Uploader) {
 
 	search.NewController(v1.Group("/search"))
 
+	connection.NewController(v1.Group("/connection"))
 	subcategory.NewController(gcpService, v1.Group("/subcategory"))
 	category.NewController(v1.Group("/category"))
 	project.NewController(gcpService, v1.Group("/project"))
