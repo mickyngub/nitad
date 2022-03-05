b22fe9ec6a3c461c2976463f98a2278e5cf3cab8
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-05 23:03:23 +0700

    refactor: Repository & Service interface

---
+++ api/subcategory/interface.go
@@ -0,0 +1,28 @@
+package subcategory
+
+import (
+	"context"
+	"mime/multipart"
+
+	"github.com/birdglove2/nitad-backend/errors"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Repository interface {
+	ListSubcategory(ctx context.Context) ([]Subcategory, errors.CustomError)
+	GetSubcategoryById(ctx context.Context, oid primitive.ObjectID) (*Subcategory, errors.CustomError)
+	AddSubcategory(ctx context.Context, subcate *Subcategory) (*Subcategory, errors.CustomError)
+	EditSubcategory(ctx context.Context, subcate *Subcategory) (*Subcategory, errors.CustomError)
+	DeleteSubcategory(ctx context.Context, oid primitive.ObjectID) errors.CustomError
+}
+
+type Service interface {
+	ListSubcategory(ctx context.Context) ([]Subcategory, errors.CustomError)
+	GetSubcategoryById(ctx context.Context, oid primitive.ObjectID) (*Subcategory, errors.CustomError)
+	AddSubcategory(ctx context.Context, files []*multipart.FileHeader, subcate *Subcategory) (*Subcategory, errors.CustomError)
+	EditSubcategory(ctx *fiber.Ctx, subcate *Subcategory) (*Subcategory, errors.CustomError)
+	DeleteSubcategory(ctx context.Context, oid primitive.ObjectID) errors.CustomError
+
+	HandleUpdateImage(ctx *fiber.Ctx, subcate *Subcategory) (*Subcategory, errors.CustomError)
+}
