b49a30f244a42086ae5862e49c959c18051f32c0
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-05 23:04:16 +0700

    refactor: controller layer

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/birdglove2/nitad-backend/api/search"
 	"github.com/birdglove2/nitad-backend/api/spatial"
 	"github.com/birdglove2/nitad-backend/api/subcategory"
+	"github.com/birdglove2/nitad-backend/database"
 	"github.com/birdglove2/nitad-backend/gcp"
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,12 +16,17 @@ import (
 const API_PREFIX = "/api/v1"
 
 func CreateAPI(app *fiber.App, gcpService gcp.Uploader) {
+	client := database.GetClient()
+	subcategoryRepository := subcategory.NewRepository(client)
+	subcategoryService := subcategory.NewService(subcategoryRepository, gcpService)
+
 	v1 := app.Group(API_PREFIX)
 
 	search.NewController(v1.Group("/search"))
 
 	connection.NewController(v1.Group("/connection"))
-	subcategory.NewController(gcpService, v1.Group("/subcategory"))
+
+	subcategory.NewController(subcategoryService, v1.Group("/subcategory"))
 	category.NewController(v1.Group("/category"))
 	project.NewController(gcpService, v1.Group("/project"))
 
