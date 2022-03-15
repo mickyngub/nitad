2c7a6550c0d7de7714187b0a029bfc60dba73aa2
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-15 15:11:35 +0700

    chore: log error throw

--- errors/main.go
+++ errors/main.go
@@ -1,6 +1,9 @@
 package errors
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
 
 type CustomError interface {
 	Code() int
@@ -8,5 +11,6 @@ type CustomError interface {
 }
 
 func Throw(c *fiber.Ctx, ce CustomError) error {
+	zap.S().Warn("Error Throw: ", ce.Code(), ": ", ce.Error())
 	return c.Status(ce.Code()).JSON(fiber.Map{"success": false, "result": ce.Error()})
 }
